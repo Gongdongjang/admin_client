4e39717d3d1cf6992c0993ca3a432a9330690941
eunjungL <dldmswjd322@gmail.com>
2022-06-15 22:21:30 +0900

    [기능추가] 사장님 회원가입 시 핸드폰 번호 인증 기능 추가

--- app/src/main/java/com/example/admin_client/SignUpActivity.java
+++ app/src/main/java/com/example/admin_client/SignUpActivity.java
@@ -3,12 +3,139 @@ package com.example.admin_client;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+
+interface SignUpService {
+    @POST("signup/id-check")
+    Call<ResponseBody> checkId(@Body JsonObject body);
+
+    @POST("signup/phone-check")
+    Call<ResponseBody> checkPhone(@Body JsonObject body);
+
+    @POST("signup/phone-check/verify")
+    Call<ResponseBody> phoneVerify(@Body JsonObject body);
+
+}
 
 public class SignUpActivity extends AppCompatActivity {
+    String TAG = SignUpActivity.class.getSimpleName();
+    Retrofit retrofit = new Retrofit.Builder()
+            .baseUrl("http://10.0.2.2:5000/api/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build();
+    SignUpService service = retrofit.create(SignUpService.class);
+    JsonParser jsonParser = new JsonParser();
+
+    Button phone_verify_btn, code_verify_btn;
+    EditText phone_number, code_verify_input;
+    TextView code_verify_txt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        phone_verify_btn = findViewById(R.id.verify_button);
+        phone_number = findViewById(R.id.phone_number_input);
+        phone_verify_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                phoneCheck(phone_number.getText().toString());
+            }
+        });
+
+        code_verify_btn = findViewById(R.id.code_verify_button);
+        code_verify_input = findViewById(R.id.code_verify_input);
+        code_verify_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                phoneVerify(code_verify_input.getText().toString(), phone_number.getText().toString());
+            }
+        });
+    }
+
+    void phoneCheck(String phone_number) {
+        JsonObject body = new JsonObject();
+        body.addProperty("phone_number", phone_number);
+
+        Call<ResponseBody> call = service.checkPhone(body);
+        call.enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                if (response.isSuccessful()) {
+                    try {
+                        JsonObject res =  (JsonObject) jsonParser.parse(response.body().string());
+                        Log.d(TAG, res.get("msg").getAsString());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                else {
+                    try {
+                        Log.d(TAG, "Fail " + response.errorBody().string());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+                Log.e(TAG, "onFailure: e " + t.getMessage());
+            }
+        });
+    }
+
+    void phoneVerify(String code, String phone_number) {
+        code_verify_txt = findViewById(R.id.code_verify_txt);
+
+        JsonObject body = new JsonObject();
+        body.addProperty("phone_number", phone_number);
+        body.addProperty("code", code);
+
+        Call<ResponseBody> call = service.phoneVerify(body);
+        call.enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                try {
+                    JsonObject res = (JsonObject) jsonParser.parse(response.body().string());
+                    if (res.get("phone_valid").getAsBoolean()) {
+                        code_verify_txt.setText("인증됐습니다.");
+                    } else {
+                        code_verify_txt.setText("다시 시도해주세요.");
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+
+            }
+        });
     }
 }
